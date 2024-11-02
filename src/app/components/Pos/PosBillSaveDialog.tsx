b8e0c9ded3d071092ced5513c06575eda35931e6
pthengtr <pthengtr@gmail.com>
2024-11-02 23:44:51 +0800

    bill creation test

--- src/app/components/Pos/PosBillSaveDialog.tsx
+++ src/app/components/Pos/PosBillSaveDialog.tsx
@@ -6,7 +6,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { PosContext, PosContextType } from "./PosProvider";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import { DialogClose } from "@radix-ui/react-dialog";
@@ -14,49 +14,108 @@ import PosBillTotalCard from "./PosBillTotalCard";
 import PosBillSaveDialogCash from "./PosBillSaveDialogCash";
 import PosBillSaveDialogTransfer from "./PosBillSaveDialogTransfer";
 import { supabase } from "@/app/lib/supabase";
+import { billType } from "../Transaction/TransactionProvider";
 
 export default function PosBillSaveDialog() {
   const { payment, currentCustomer, vat, returnMode } = useContext(
     PosContext
   ) as PosContextType;
 
-  const [billCount, setBillCount] = useState(0);
+  function formatNewBill(date: Date, data: billType[]) {
+    let billHeader, newBillType;
+    if (vat === "vat") {
+      if (payment === "CASH") {
+        billHeader = "RT";
+        newBillType = "1SY";
+      } else if (payment === "CREDIT") {
+        billHeader = "IVT";
+        newBillType = "1SN";
+      }
+    } else if (vat === "novat") {
+      billHeader = "NOVAT";
+    }
 
-  console.log(billCount);
+    const newBillId = Math.random().toString().substring(2, 14);
 
-  async function getVatBillCount(year: string, month: string) {
-    const { data, error, count } = await supabase
+    const sequenceNumber =
+      data.length === 0
+        ? "0001"
+        : (parseInt(data[0].BILLNO.split("-")[1]) + 1)
+            .toString()
+            .padStart(4, "0");
+
+    const newBillNo =
+      billHeader +
+      date
+        .toLocaleDateString("th-TH", { month: "2-digit", year: "2-digit" })
+        .split("/")
+        .reverse()
+        .join("") +
+      "-" +
+      sequenceNumber;
+
+    const newBill = {
+      billId: newBillId,
+      BILLNO: newBillNo,
+      BILLTYPE: newBillType,
+      JOURDATE: date,
+      BILLDATE: date,
+      BEFORETAX: 1,
+      TAX: 1,
+      AFTERTAX: 2,
+      REMARKS: "Test Bill Creation",
+      SALE: "Test User",
+    };
+
+    return newBill;
+  }
+
+  async function createNewBill(date: Date) {
+    const { data, error } = await supabase
       .from("bills")
-      .select(`*`, { count: "exact" })
+      .select(`*`)
       .neq("TAX", `0`)
-      .ilike("BILLTYPE", `%S%`)
+      .ilike("BILLTYPE", `1SY`)
       .lt(
         "BILLDATE",
-        new Date(`${year}-${month}-01 00:00`).toLocaleDateString("en-US")
+        new Date(
+          `${date.getUTCFullYear().toString()}-${(
+            date.getUTCMonth() + 2
+          ).toString()}-01 00:00`
+        ).toLocaleDateString("en-US")
       )
       .gte(
         "BILLDATE",
-        new Date(`${year}-${month}-01 00:00`).toLocaleDateString("en-US")
+        new Date(
+          `${date.getUTCFullYear().toString()}-${(
+            date.getUTCMonth() + 1
+          ).toString()}-01 00:00`
+        ).toLocaleDateString("en-US")
       )
       .order("BILLDATE", { ascending: false })
       .limit(1);
 
-    console.log(data);
-    console.log(count);
+    console.log("get latest", data);
 
     if (error) return;
-    if (count !== null) setBillCount(count);
+    // if (data !== null) setLatestBill(data);
+    // if (count !== null) setBillCount(count);
+    const newBill = formatNewBill(date, data);
+
+    console.log("input", newBill);
+
+    const { data: out, error: outerr } = await supabase
+      .from("bills")
+      .insert([newBill])
+      .select();
+
+    if (outerr) return;
+
+    console.log("output", out);
   }
 
   function handleConfirmBill() {
-    const date = new Date();
-
-    const year = date.getUTCFullYear().toString();
-    const month = (date.getUTCMonth() + 1).toString();
-
-    console.log(year, month);
-
-    getVatBillCount(year, month);
+    createNewBill(new Date());
   }
 
   return (
