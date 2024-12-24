d60b8e0fa49ca16091ad21dd1f64684d1937e0b3
pthengtr <pthengtr@gmail.com>
2024-12-24 13:06:05 +0800

    update supabase env

--- src/app/lib/supabase.tsx
+++ src/app/lib/supabase.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://jdzitzsucntqbjvwiwxm.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impkeml0enN1Y250cWJqdndpd3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyNDExNzIsImV4cCI6MjA0MjgxNzE3Mn0.NmBkpUpX939nzVOq2MzgpXNFGmYTz7FZQZvgVSOqKY4"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
